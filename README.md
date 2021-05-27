a35f6a9d434a9fb9971c3520c433804967615dfc
onioneffect <admin@explosive.love>
2021-05-27 13:18:58 -0400

    Big commit. Lots of experimental changes.
    * Add options --mlevel, --quiet, --once, and --split
    * --mlevel and --split are still not implemented
    * Adds an epilog to the --help message with a quick rundown of how the --mlevel option should work
    * Changes program's description in the argparse package
    * Includes many other minor changes

--- README.md
+++ README.md
@@ -8,20 +8,37 @@ Python script to download all images/webms of a 4chan thread
 The main script is called inb4404.py and can be called like this: `python inb4404.py [thread/filename]`
 
 ```
-usage: inb4404.py [-h] [-c] [-d] [-l] [-n] [-r] thread
+usage: inb4404.py [-h] [-c] [-d] [-l] [-m MLEVEL] [-n] [-q] [-o] [-r]
+                  [-s SPLIT]
+                  thread
+
+Python script to download all images/webms of a 4chan thread
 
 positional arguments:
   thread              url of the thread (or filename; one url per line)
 
 optional arguments:
-  -h, --help          show this help message and exit
-  -c, --with-counter  show a counter next the the image that has been
-                      downloaded
-  -d, --date          show date as well
-  -l, --less          show less information (surpresses checking messages)
-  -n, --use-names     use thread names instead of the thread ids
-                      (...4chan.org/board/thread/thread-id/thread-name)
-  -r, --reload        reload the queue file every 5 minutes
+  -h, --help            show this help message and exit
+  -c, --with-counter    show a counter next the the image that has been
+                        downloaded
+  -d, --date            show date as well
+  -l, --less            show less information (supresses checking messages)
+  -m MLEVEL, --mlevel MLEVEL
+                        define how strictly to manage threads file (TODO)
+  -n, --use-names       use thread names instead of the thread ids
+                        (...4chan.org/board/thread/thread-id/thread-name)
+  -q, --quiet           suppress all other logging options, only shows errors
+  -o, --once            only check each thread once and quit (supresses
+                        --reload)
+  -r, --reload          reload the queue file every 5 minutes
+  -s SPLIT, --split SPLIT
+                        choose substring to separate thread URLs in threads
+                        file. (default: \n)
+
+MLEVEL value:        Behaviour:
+  0                     Remove 404'd threads from file
+  1                     Remove archived threads
+  2                     Remove archived threads, while backing up file (recommended)
 ```
 
 You can parse a file instead of a thread url. In this file you can put as many links as you want, you just have to make sure that there's one url per line. A line is considered to be a url if the first 4 letters of the line start with 'http'.
@@ -39,3 +56,8 @@ then it looks for all threads that include `mhg` inside the `vg` board, stores t
 ### Legacy ###
 
 The current scripts are written in python3, in case you still use python2 you can use an old version of the script inside the legacy directory.
+
+### Changes ###
+
+This copy of the program includes many changes made by [onioneffect](https://github.com/onioneffect) on github, including the `-mqos` options and some minor changes in the program's logic. Some of these features are still in development and may not work very well!
+
--- inb4404.py
+++ inb4404.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import urllib.request, urllib.error, urllib.parse, argparse, logging
 import os, re, time
 import http.client 
@@ -9,21 +10,66 @@ log = logging.getLogger('inb4404')
 workpath = os.path.dirname(os.path.realpath(__file__))
 args = None
 
+def mlevel_msg():
+    cool = [
+        "Remove 404'd threads from file",
+        "Remove archived threads",
+        "Remove archived threads, while backing up file (recommended)"
+    ]
+
+    ret = """MLEVEL value:        Behaviour:
+"""
+    for i, j in enumerate(cool):
+        ret += "  {0}                     {1}\n".format(i, j)
+
+    return ret
+
 def main():
     global args
-    parser = argparse.ArgumentParser(description='inb4404')
+    parser = argparse.ArgumentParser(
+        description='Python script to download all images/webms of a 4chan thread',
+        epilog=mlevel_msg(),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
     parser.add_argument('thread', nargs=1, help='url of the thread (or filename; one url per line)')
     parser.add_argument('-c', '--with-counter', action='store_true', help='show a counter next the the image that has been downloaded')
     parser.add_argument('-d', '--date', action='store_true', help='show date as well')
-    parser.add_argument('-l', '--less', action='store_true', help='show less information (surpresses checking messages)')
+    parser.add_argument('-l', '--less', action='store_true', help='show less information (supresses checking messages)')
+    parser.add_argument('-m', '--mlevel', action='store', help='define how strictly to manage threads file') # TODO
     parser.add_argument('-n', '--use-names', action='store_true', help='use thread names instead of the thread ids (...4chan.org/board/thread/thread-id/thread-name)')
+    parser.add_argument('-q', '--quiet', action='store_true', help='suppress all other logging options, only shows errors')
+    parser.add_argument('-o', '--once', action='store_true', help='only check each thread once and quit (supresses --reload)')
     parser.add_argument('-r', '--reload', action='store_true', help='reload the queue file every 5 minutes')
+    #TODO: The two options below this line are also not implemented.
+    parser.add_argument('-s', '--split', action='store', help='choose substring to separate thread URLs in threads file (default: \\n)')
+    parser.add_argument('-t', '--title', action='store_true', help='save files with original filename')
     args = parser.parse_args()
 
+    # TODO: Organize these if statements!
+    # Some of them would be better as `elif`s!
+    if args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+        args.less = False
+        args.date = False
+
+    if args.once:
+        args.reload = False
+
+    if args.title:
+        try:
+            import bs4
+        except ImportError:
+            log.warning("BeautifulSoup4 not found! Disabling --title option!")
+            args.title = False
+
+    chan_fmt = '%I:%M:%S %p'
     if args.date:
-        logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
-    else:
-        logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%I:%M:%S %p')    
+        chan_fmt = '%Y-%m-%d ' + chan_fmt
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt=chan_fmt)
+
+    if args.mlevel:
+        log.info('The --mlevel function is still in development, so enabling it has no effect!')
 
     thread = args.thread[0].strip()
     if thread[:4].lower() == 'http':
@@ -48,14 +94,22 @@ def download_thread(thread_link, args):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    # TODO: Wrap my head around this loop. :woozy:
     while True:
+        if not args.less:
+            log.info('Checking ' + board + '/' + thread)
+
         try:
+            page_html = load(thread_link).decode('utf-8')
+
             regex = '(\/\/i(?:s|)\d*\.(?:4cdn|4chan)\.org\/\w+\/(\d+\.(?:jpg|png|gif|webm)))'
-            regex_result = list(set(re.findall(regex, load(thread_link).decode('utf-8'))))
+            regex_result = list(set(re.findall(regex, page_html)))
             regex_result = sorted(regex_result, key=lambda tup: tup[1])
             regex_result_len = len(regex_result)            
             regex_result_cnt = 1
 
+            # TODO: Maybe turn this into an enumerate?
+            # See line 118
             for link, img in regex_result:
                 img_path = os.path.join(directory, img)
                 if not os.path.exists(img_path):
@@ -65,13 +119,14 @@ def download_thread(thread_link, args):
                     if args.with_counter:
                         output_text = '[' + str(regex_result_cnt).rjust(len(str(regex_result_len))) +  '/' + str(regex_result_len) + '] ' + output_text
 
-                    log.info(output_text)
+                    if not args.quiet:
+                        log.info(output_text)
 
                     with open(img_path, 'wb') as f:
                         f.write(data)
 
                     ##################################################################################
-                    # saves new images to a seperate directory
+                    # saves new images to a separate directory
                     # if you delete them there, they are not downloaded again
                     # if you delete an image in the 'downloads' directory, it will be downloaded again
                     copy_directory = os.path.join(workpath, 'new', board, thread)
@@ -83,24 +138,38 @@ def download_thread(thread_link, args):
                     ##################################################################################
                 regex_result_cnt += 1
 
+            if args.once:
+                break
+
         except urllib.error.HTTPError as err:
             time.sleep(10)
             try:
-                load(thread_link)    
+                resp = load(thread_link)
             except urllib.error.HTTPError as err:
-                log.info('%s 404\'d', thread_link)
+                if err.code == 404:
+                    log.info(thread_link + ' 404\'d')
+                else:
+                    log.error('Something went very wrong! Response code: ' + str(err.code))
+
                 break
             continue
         except (urllib.error.URLError, http.client.BadStatusLine, http.client.IncompleteRead):
-            if not args.less:
-                log.warning('Something went wrong')
+            log.error('Something went wrong')
 
-        if not args.less:
-            log.info('Checking ' + board + '/' + thread)
         time.sleep(20)
 
 def download_from_file(filename):
-    running_links = []
+    # TODO: Finish all of this...
+    running_links = list()
+    split_text = list()
+    fptr = open(filename)
+
+    if args.split == None:
+        for line in fptr:
+            split_text += line
+    else:
+        split_text = fptr.read().split(args.split)
+
     while True:
         processes = []
         for link in [_f for _f in [line.strip() for line in open(filename) if line[:4] == 'http'] if _f]:
@@ -118,6 +187,7 @@ def download_from_file(filename):
         if args.reload:
             time.sleep(60 * 5) # 5 minutes
             links_to_remove = []
+            # Find out what all this does VVVVVVV
             for process, link in processes:
                 if not process.is_alive():
                     links_to_remove.append(link)
@@ -133,6 +203,8 @@ def download_from_file(filename):
                 log.info('Reloading ' + args.thread[0]) # thread = filename here; reloading on next loop
         else:
             break
+
+    fptr.close()
 
 
 if __name__ == '__main__':
